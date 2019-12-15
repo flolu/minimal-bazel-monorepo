c296069af360889d3f17ff7d57b13d630ca98c28
flolude <fllu@pm.me>
2019-12-15 21:59:23 +0100

    clean up work

--- server/index.ts
+++
@@ -1,5 +0,0 @@
-import { name } from './util';
-
-console.log('name is :'+ name);
-console.log('server starting...');
-console.log('hello ', name);
---
+++ server/src/index.ts
@@ -0,0 +1,6 @@
+import { add } from './util';
+
+const a = 13;
+const b = 29;
+
+console.log(`${a} + ${b} is ${add(a, b)}`);
