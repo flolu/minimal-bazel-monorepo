f449dda3b2e09f21beeeecbfd4bc1868ec1f3152
flolude <fllu@pm.me>
2019-12-18 17:32:40 +0100

    add express server

--- services/server/index.ts
+++ services/server/index.ts
@@ -1,12 +1,26 @@
 import * as faker from 'faker';
+import * as express from 'express';
 
 import { Constants } from '@lbm/enums';
 
 import { add } from './util';
 
-const a = 13;
-const b = 29;
+export const start = () => {
+  const app = express();
 
-console.log(`${a} + ${b} is ${add(a, b)}`);
-console.log(`${Constants.Pi} + ${Constants.E} is ${add(Number(Constants.Pi), Number(Constants.E))}`);
-console.log('faker', faker.name.firstName());
+  app.get('/alive', (req, res) => res.send('alive'));
+
+  app.get('/name', (req, res) => {
+    const name = faker.name.findName();
+    res.send(`My name is ${name}`);
+  });
+
+  app.get('**', (req, res) => {
+    const result = add(Number(Constants.Pi), Number(Constants.E));
+    res.send(`${Constants.Pi} + ${Constants.E} = ${result}`);
+  });
+
+  app.listen(3000, () => console.log('gateway listening on port 3000'));
+};
+
+start();
